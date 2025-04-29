2906d9d77f0556f0fda1239739c960252b64c468
elijahxparmalee <elijahleeofficial@gmail.com>
2025-04-28 23:45:47 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ def log(string):
     sys.stderr.write("{}\n".format(string))
     sys.stderr.flush()
 
-def main():
+def solveMaze():
     log("Running...")
     API.setColor(0, 0, "G")
     API.setText(0, 0, "abc")
@@ -16,5 +16,4 @@ def main():
             API.turnRight()
         API.moveForward()
 
-if __name__ == "__main__":
-    main()
+
